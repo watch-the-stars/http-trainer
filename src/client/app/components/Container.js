ef71788768b8eb19c6689b714066cf664063c27c
risesk <antonkuvichka@gmail.com>
2020-01-07 13:37:05 +0300

    sticky footer

--- src/client/app/components/Container.js
+++ src/client/app/components/Container.js
@@ -4,10 +4,12 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  
+  box-sizing:border-box;
+
   margin: 0 auto;  
-  padding 120px 0 30px;
+  padding 30px 0 30px;
   max-width: 555px;
+  min-height: 100vh;
 `;
 
 export default Container;
--- src/client/app/components/YaPracticumLogo/Logo.js
+++ src/client/app/components/YaPracticumLogo/Logo.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
 
 const Logo = styled.a`
-  margin-top: 176px;
+  margin-top: auto;
+  padding-top: 60px;
 `;
 
 export default Logo;
